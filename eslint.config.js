7441bcc27cd543880e05dea43ab0693026621a23
Henrik Gemal <henrik@gemal.dk>
2026-07-07 13:16:01 +0200

    fixes

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@ import js from '@eslint/js';
 import n from 'eslint-plugin-n';
 
 export default [
-    { ignores: ['eslint.config.js'] },
     js.configs.recommended,
     n.configs['flat/recommended'],
     {
